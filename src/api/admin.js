ba544bb7a967a8c3b683095efa2a49c2832efa24
xuxian316 <3331484470@qq.com>
2026-03-28 17:38:30 +0800

    feat: 添加文章页面功能，更新知识页面样式和分类数据处理

--- src/api/admin.js
+++ src/api/admin.js
@@ -7,3 +7,6 @@ export function login(data) {
 export function categoryTree() {
     return service.get("/knowledge/category/tree");
 }
+export function articlePage (params) {
+    return service.get("/knowledge/article/page",{params});
+}
--- src/views/Knowledge.vue
+++ src/views/Knowledge.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <pagehead title="知识文章" margin-bottom="20px">
+        <pagehead title="知识文章" class="head">
             <template #buttons>
                 <el-button type="primary">新增</el-button>
             </template>
@@ -15,23 +15,49 @@
     </div>
 </template>
 <script setup>
-import{onMounted,ref} from 'vue'
+import{onMounted,ref,reactive} from 'vue'
 import pagehead from "@/components/pagehead.vue"
 import TableSearch from "../components/TableSearch.vue";
 import { categoryTree } from "@/api/admin";
+import { articlePage } from '../api/admin';
 
-const formItem = [
+const formItem =ref([
     {comp:"input", prop:'title', label:'文章标题', placeholder:'请输入文章标题'},
     {comp:"select", prop:'category', label:'文章分类', placeholder:'请选择文章分类'},
+    {comp:"select", prop:'status', label:'文章状态', placeholder:'请选择文章状态',options:[
+        {value:"0",label:'草稿'},
+        {value:"1",label:'已发布'},
+        {value:"2",label:'已下架'},
+    ]},
 
-]
+])
 const handleSearch = (formData) => {
     // console.log(formData);
 }
+
+
+const categoryMap=reactive({})//创建一个响应式对象来存储分类数据
+const categories=ref([])//创建一个ref来存储分类数据，ref是一个对象，value属性存储实际的值
+
 onMounted(async()=>{
-    categoryTree().then(data=>{
-        console.log(data);
+    const data =await categoryTree()
+    categories.value=data.map(item=>{
+            categoryMap[item.id]=item.categoryName//将分类数据存储到categoryMap对象中，key为分类id，value为分类名称
+       return{
+            label:item.categoryName,
+            value: item.id
+       }
+       
     })
+    formItem.value[1].options=categories.value//将分类数据存储到formItem中，供select组件使用
+    console.log(formItem[1].options);
 })
 
 </script>
+
+
+<style lang="scss" scoped>
+.head{
+    margin-bottom: 20px;
+}
+</style>
