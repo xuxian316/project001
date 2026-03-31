3d960ee8c67e0c1b174d753795ff52d77dfd95c3
xuxian316 <3331484470@qq.com>
2026-04-01 02:21:35 +0800

    feat: 更新知识页面，添加分类数据处理和搜索功能

--- src/views/Knowledge.vue
+++ src/views/Knowledge.vue
@@ -4,6 +4,7 @@
             <template #buttons>
                 <el-button type="primary">新增</el-button>
             </template>
+            
         </pagehead>
         <TableSearch :formItem="formItem" @search="handleSearch"/>
 
@@ -21,18 +22,42 @@ import TableSearch from "../components/TableSearch.vue";
 import { categoryTree } from "@/api/admin";
 import { articlePage } from '../api/admin';
 
-const formItem =ref([
-    {comp:"input", prop:'title', label:'文章标题', placeholder:'请输入文章标题'},
-    {comp:"select", prop:'category', label:'文章分类', placeholder:'请选择文章分类'},
-    {comp:"select", prop:'status', label:'文章状态', placeholder:'请选择文章状态',options:[
-        {value:"0",label:'草稿'},
-        {value:"1",label:'已发布'},
-        {value:"2",label:'已下架'},
-    ]},
-
+const formItem = ref([
+    { comp: "input", prop: 'title', label: '文章标题', placeholder: '请输入文章标题' },
+    { 
+        comp: "select", prop: 'category', label: '文章分类', 
+        placeholder: '请选择文章分类', 
+        options: []  // ✅ 加上初始空数组，避免渲染时读取 undefined
+    },
+    { 
+        comp: "select", prop: 'status', label: '文章状态', 
+        placeholder: '请选择文章状态',
+        options: [
+            { value: "0", label: '草稿' },
+            { value: "1", label: '已发布' },
+            { value: "2", label: '已下架' },
+        ]
+    },
 ])
-const handleSearch = (formData) => {
-    // console.log(formData);
+
+//分页参数
+
+const pagination=reactive({
+    currentPage:1,
+    size:10,
+    total:0
+})
+const handleSearch =async (formData) => {
+    console.log(formData,"查询参数")
+
+    const params = {
+        ...pagination,
+        ...formData
+    }
+
+
+    const data = await articlePage(params)
+    console.log(data,"文章列表")
 }
 
 
@@ -42,15 +67,18 @@ const categories=ref([])//创建一个ref来存储分类数据，ref是一个对
 onMounted(async()=>{
     const data =await categoryTree()
     categories.value=data.map(item=>{
-            categoryMap[item.id]=item.categoryName//将分类数据存储到categoryMap对象中，key为分类id，value为分类名称
-       return{
+        categoryMap[item.id]=item.categoryName//将分类数据存储到categoryMap对象中，key为分类id，value为分类名称
+        return{
             label:item.categoryName,
             value: item.id
-       }
-       
+        }
     })
     formItem.value[1].options=categories.value//将分类数据存储到formItem中，供select组件使用
-    console.log(formItem[1].options);
+
+
+    //获取列表
+    handleSearch()
+    
 })
 
 </script>
